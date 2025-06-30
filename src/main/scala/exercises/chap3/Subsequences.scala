1485d8deaa4d3c36761e67a8f1f6d68a19eb4977
sebastian <sps@dixa.com>
2025-06-30 16:47:37 +0200

    tree map

--- src/main/scala/exercises/chap3/Subsequences.scala
+++ src/main/scala/exercises/chap3/Subsequences.scala
@@ -2,18 +2,17 @@ import exercises.chap3.FpList
 import exercises.chap3.FpList.*
 
 def hasSubsequence[A](seq: FpList[A], sub: FpList[A]): Boolean =
-    @annotation.tailrec
-    def loop[A](l: FpList[A], pfx: FpList[A]): Boolean =
-        (l, pfx) match
-            case (_, Nil) => true
-            case (Cons(h, t), Cons(subh, subt)) if h == subh => loop(t, subt)
-            case _ => false
+  @annotation.tailrec
+  def loop[A](l: FpList[A], pfx: FpList[A]): Boolean =
+    (l, pfx) match
+      case (_, Nil)                                    => true
+      case (Cons(h, t), Cons(subh, subt)) if h == subh => loop(t, subt)
+      case _                                           => false
 
-    seq match
-        case Nil => sub == Nil
-        case _ if loop(seq, sub) => true
-        case Cons(h, t) => hasSubsequence(t, sub)
-        
+  seq match
+    case Nil                 => sub == Nil
+    case _ if loop(seq, sub) => true
+    case Cons(h, t)          => hasSubsequence(t, sub)
 
 @main def matchSubsequences(): Unit =
   println(hasSubsequence(FpList(1, 2, 3, 4), FpList(1, 2)))
--- src/main/scala/exercises/chap3/Tree.scala
+++ src/main/scala/exercises/chap3/Tree.scala
@@ -5,5 +5,22 @@ enum Tree[+A]:
   case Branch(left: Tree[A], right: Tree[A])
 
   def size: Int = this match
-    case Leaf(_) => 1
+    case Leaf(_)      => 1
     case Branch(l, r) => 1 + l.size + r.size
+
+  def depth: Int = this match
+    case Leaf(_)      => 0
+    case Branch(l, r) => 1 + l.depth.max(r.depth)
+
+object Tree:
+
+  extension (t: Tree[Int])
+
+    def maximum: Int =
+      t match
+        case Leaf(i)      => i
+        case Branch(l, r) => l.maximum.max(r.maximum)
+
+  def map[A, B](t: Tree[A], f: A => B): Tree[B] = t match
+    case Leaf(v)      => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l, f), map(r, f))
